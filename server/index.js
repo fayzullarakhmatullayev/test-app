f6078d9481b4ec53c458eb28b944eb87e0ae2729
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2023-10-04 20:25:18 +0500

    vercel

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import pool from '../db.js';
+import pool from './db.js';
 
 dotenv.config();
 const app = express();
