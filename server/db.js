36e39c2ab61877566b03a9f10084ac5ef09f4f03
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2023-10-04 18:56:44 +0500

    versel

--- server/db.js
+++ server/db.js
@@ -5,11 +5,7 @@ const { Pool } = pg;
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 // pending, processed, delivered, closed
