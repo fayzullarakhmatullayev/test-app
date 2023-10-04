d464513a21919fd3ad1cd0a44366a0f627563364
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2023-10-04 19:29:06 +0500

    versel

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'success' });
+});
+
 app.get('/api/employee', async (req, res) => {
   try {
     pool.query('SELECT * FROM employee', (error, results) => {
